afb1eab121b1cb0ab5417542c229981faf387eb0
Junior Ales <edilson.ales.jr@gmail.com>
2016-08-05 17:55:47 +0100

    making player immutable

--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -1,26 +1,39 @@
-var TennisGame1 = function(player1Name, player2Name) {
-  this.player1 = new Player(player1Name);
-  this.player2 = new Player(player2Name);
-};
+var TennisGame1 = (function() {
+  function Player(name) {
+    var INITIAL_SCORE = 0;
 
-function Player(name) {
-  this.name = name;
-  this.score = 0;
-}
+    var score = INITIAL_SCORE;
 
+    var self = {
+      getName: function() { return name; },
+      getScore: function() { return score; },
+      scored: function() { score++; }
+    };
+
+    Object.freeze(self);
+    return self;
+  }
+
+  return function(player1Name, player2Name) {
+    this.player1 = new Player(player1Name);
+    this.player2 = new Player(player2Name);
+  };
+
+})();
 
 TennisGame1.prototype.wonPoint = function(playerName) {
-  if (playerName === this.player1.name)
-    this.player1.score += 1;
-  if (playerName === this.player2.name)
-    this.player2.score += 1;
+  if (playerName === this.player1.getName())
+    this.player1.scored();
+  if (playerName === this.player2.getName())
+    this.player2.scored();
 };
+
 
 TennisGame1.prototype.getScore = function() {
   var score = "";
   var tempScore = 0;
-  if (this.player1.score === this.player2.score) {
-    switch (this.player1.score) {
+  if (this.player1.getScore() === this.player2.getScore()) {
+    switch (this.player1.getScore()) {
       case 0:
         score = "Love-All";
         break;
@@ -34,18 +47,18 @@ TennisGame1.prototype.getScore = function() {
         score = "Deuce";
         break;
     }
-  } else if (this.player1.score >= 4 || this.player2.score >= 4) {
-    var minusResult = this.player1.score - this.player2.score;
-    if (minusResult === 1) score = "Advantage " + this.player1.name;
-    else if (minusResult === -1) score = "Advantage " + this.player2.name;
-    else if (minusResult >= 2) score = "Win for " + this.player1.name;
-    else score = "Win for " + this.player2.name;
+  } else if (this.player1.getScore() >= 4 || this.player2.getScore() >= 4) {
+    var minusResult = this.player1.getScore() - this.player2.getScore();
+    if (minusResult === 1) score = "Advantage " + this.player1.getName();
+    else if (minusResult === -1) score = "Advantage " + this.player2.getName();
+    else if (minusResult >= 2) score = "Win for " + this.player1.getName();
+    else score = "Win for " + this.player2.getName();
   } else {
     for (var i = 1; i < 3; i++) {
-      if (i === 1) tempScore = this.player1.score;
+      if (i === 1) tempScore = this.player1.getScore();
       else {
         score += "-";
-        tempScore = this.player2.score;
+        tempScore = this.player2.getScore();
       }
       switch (tempScore) {
         case 0:
