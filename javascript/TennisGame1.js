e0ec6638ef18898d47f2e2e778898031b7e8b7b0
Junior Ales <edilson.ales.jr@gmail.com>
2016-08-06 18:07:37 +0100

    hiding player factory  and removing switch statement

--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -1,24 +1,30 @@
-var PlayerFactory = (function() {
-  var Player = {
-    init: function(name) {
-      this.name = name;
-      this.score = 0;
-      return this;
-    },
-
-    scored: function() { this.score++; }
-  };
-
-  Object.freeze(Player);
-
-  return {
-    create: function(name) {
-      return Object.create(Player).init(name);
-    }
-  };
-})();
-
 var TennisGame1 = (function() {
+  var PlayerFactory = (function() {
+    var Player = {
+      init: function(name) {
+        this.name = name;
+        this.score = 0;
+        return this;
+      },
+
+      scored: function() { this.score++; }
+    };
+
+    Object.freeze(Player);
+
+    return {
+      create: function(name) {
+        return Object.create(Player).init(name);
+      }
+    };
+  })();
+
+  var SCORES = {
+    0: "Love",
+    1: "Fifteen",
+    2: "Thirty"
+  };
+
   var Tennis = {
     init: function(player1name, player2name) {
       this.player1 = PlayerFactory.create(player1name);
@@ -36,27 +42,25 @@ var TennisGame1 = (function() {
     getScore: function() {
       var score = "";
       var tempScore = 0;
+
       if (this.player1.score === this.player2.score) {
-        switch (this.player1.score) {
-        case 0:
-          score = "Love-All";
-          break;
-        case 1:
-          score = "Fifteen-All";
-          break;
-        case 2:
-          score = "Thirty-All";
-          break;
-        default:
+        if (SCORES[this.player1.score]) {
+          score = SCORES[this.player1.score] + "-All";
+        } else {
           score = "Deuce";
-          break;
         }
       } else if (this.player1.score >= 4 || this.player2.score >= 4) {
         var minusResult = this.player1.score - this.player2.score;
-        if (minusResult === 1) score = "Advantage " + this.player1.name;
-        else if (minusResult === -1) score = "Advantage " + this.player2.name;
-        else if (minusResult >= 2) score = "Win for " + this.player1.name;
-        else score = "Win for " + this.player2.name;
+
+        if (minusResult === 1) {
+          score = "Advantage " + this.player1.name;
+        } else if (minusResult === -1) {
+          score = "Advantage " + this.player2.name;
+        } else if (minusResult >= 2){
+          score = "Win for " + this.player1.name;
+        } else {
+          score = "Win for " + this.player2.name;
+        }
       } else {
         for (var i = 1; i < 3; i++) {
           if (i === 1) tempScore = this.player1.score;
