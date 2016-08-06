5254174c9d43d447c82ad4359588201620da76d1
Junior Ales <edilson.ales.jr@gmail.com>
2016-08-06 17:44:03 +0100

    moved from class(ish) to OLOO

--- javascript/TennisGame1.js
+++ javascript/TennisGame1.js
@@ -1,83 +1,98 @@
-var TennisGame1 = (function() {
-  function Player(name) {
-    var INITIAL_SCORE = 0;
+var PlayerFactory = (function() {
+  var Player = {
+    init: function(name) {
+      this.name = name;
+      this.score = 0;
+      return this;
+    },
 
-    var score = INITIAL_SCORE;
-
-    var self = {
-      getName: function() { return name; },
-      getScore: function() { return score; },
-      scored: function() { score++; }
-    };
-
-    Object.freeze(self);
-    return self;
-  }
-
-  return function(player1Name, player2Name) {
-    this.player1 = new Player(player1Name);
-    this.player2 = new Player(player2Name);
+    scored: function() { this.score++; }
   };
 
+  Object.freeze(Player);
+
+  return {
+    create: function(name) {
+      return Object.create(Player).init(name);
+    }
+  };
 })();
 
-TennisGame1.prototype.wonPoint = function(playerName) {
-  if (playerName === this.player1.getName())
-    this.player1.scored();
-  if (playerName === this.player2.getName())
-    this.player2.scored();
-};
+var TennisGame1 = (function() {
+  var Tennis = {
+    init: function(player1name, player2name) {
+      this.player1 = PlayerFactory.create(player1name);
+      this.player2 = PlayerFactory.create(player2name);
+      return this;
+    },
 
+    wonPoint: function(playerName) {
+      if (playerName === this.player1.name)
+        this.player1.scored();
+      if (playerName === this.player2.name)
+        this.player2.scored();
+    },
 
-TennisGame1.prototype.getScore = function() {
-  var score = "";
-  var tempScore = 0;
-  if (this.player1.getScore() === this.player2.getScore()) {
-    switch (this.player1.getScore()) {
-      case 0:
-        score = "Love-All";
-        break;
-      case 1:
-        score = "Fifteen-All";
-        break;
-      case 2:
-        score = "Thirty-All";
-        break;
-      default:
-        score = "Deuce";
-        break;
-    }
-  } else if (this.player1.getScore() >= 4 || this.player2.getScore() >= 4) {
-    var minusResult = this.player1.getScore() - this.player2.getScore();
-    if (minusResult === 1) score = "Advantage " + this.player1.getName();
-    else if (minusResult === -1) score = "Advantage " + this.player2.getName();
-    else if (minusResult >= 2) score = "Win for " + this.player1.getName();
-    else score = "Win for " + this.player2.getName();
-  } else {
-    for (var i = 1; i < 3; i++) {
-      if (i === 1) tempScore = this.player1.getScore();
-      else {
-        score += "-";
-        tempScore = this.player2.getScore();
-      }
-      switch (tempScore) {
+    getScore: function() {
+      var score = "";
+      var tempScore = 0;
+      if (this.player1.score === this.player2.score) {
+        switch (this.player1.score) {
         case 0:
-          score += "Love";
+          score = "Love-All";
           break;
         case 1:
-          score += "Fifteen";
+          score = "Fifteen-All";
           break;
         case 2:
-          score += "Thirty";
+          score = "Thirty-All";
           break;
-        case 3:
-          score += "Forty";
+        default:
+          score = "Deuce";
           break;
+        }
+      } else if (this.player1.score >= 4 || this.player2.score >= 4) {
+        var minusResult = this.player1.score - this.player2.score;
+        if (minusResult === 1) score = "Advantage " + this.player1.name;
+        else if (minusResult === -1) score = "Advantage " + this.player2.name;
+        else if (minusResult >= 2) score = "Win for " + this.player1.name;
+        else score = "Win for " + this.player2.name;
+      } else {
+        for (var i = 1; i < 3; i++) {
+          if (i === 1) tempScore = this.player1.score;
+          else {
+            score += "-";
+            tempScore = this.player2.score;
+          }
+          switch (tempScore) {
+          case 0:
+            score += "Love";
+            break;
+          case 1:
+            score += "Fifteen";
+            break;
+          case 2:
+            score += "Thirty";
+            break;
+          case 3:
+            score += "Forty";
+            break;
+          }
+        }
       }
+      return score;
     }
-  }
-  return score;
-};
+  };
+
+  Object.freeze(Tennis);
+
+  return {
+    init: function(player1name, player2name) {
+      return Object.create(Tennis).init(player1name, player2name);
+    },
+    type: 'oloo'
+  };
+})();
 
 if (typeof window === "undefined") {
   module.exports = TennisGame1;
